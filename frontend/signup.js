2efc367636531f2d1001a9fcae741bc1f6a77181
AahanaG411 <aahanagorantla@gmail.com>
2025-06-04 09:08:48 -0400

    Update signup.js

--- frontend/signup.js
+++ frontend/signup.js
@@ -4,7 +4,6 @@ const firebaseConfig = await res.json();
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, fetchSignInMethodsForEmail } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { firebaseConfig } from "./firebase.js";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
